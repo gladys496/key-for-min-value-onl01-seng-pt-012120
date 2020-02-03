63a7cca048f0a731affbd97d46b3dc78af03d98a
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-03 10:00:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
+weather = {:monday => 55, :tuesday => 45, :wednesday => 65}
 def key_for_min_value(name_hash)
   lowest_key = nil 
   lowest_value = nil 
